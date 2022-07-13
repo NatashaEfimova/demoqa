c1311967b71612449072bddd66f6fa522f0b9324
NatashaEfimova <102250449+NatashaEfimova@users.noreply.github.com>
2022-07-13 11:46:23 +0300

    11-46

---
+++ src/test/java/StartTest.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class StartTest {
+    WebDriver wd;
+
+    @BeforeMethod
+    public void preCondition() {
+        wd = new ChromeDriver();
+        wd.navigate().to("https://demoqa.com/");
+    }
+    @Test
+    public void testName(){}
+    @AfterMethod
+    public void postCondition() {
+
+        // wd.quit();
+    }
+}
